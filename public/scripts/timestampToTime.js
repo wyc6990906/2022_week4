c37ff84ca99347dd86dd0b628dbb3d6c5347a085
Dylan Wang <wyc6990906@hotmail.com>
2022-06-23 09:17:10 -0600

    format timestampToTime

--- public/scripts/timestampToTime.js
+++ public/scripts/timestampToTime.js
@@ -1,15 +1,14 @@
-
 export default function (timestamp) {
-      const date = new Date(timestamp);
-      const Y = date.getFullYear() + "-";
-      const M =
-        (date.getMonth() + 1 < 10
-          ? "0" + (date.getMonth() + 1)
-          : date.getMonth() + 1) + "-";
-      const D = date.getDate() + " ";
-      const hh = date.getHours() + ":";
-      const mm = date.getMinutes() + ":";
-      const ss = date.getSeconds();
-      return Y + M + D + hh + mm + ss;
+  const date = new Date(timestamp);
+  const Y = date.getFullYear() + "-";
+  const M =
+    (date.getMonth() + 1 < 10
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1) + "-";
+  const D = date.getDate() + " ";
+  const hh = date.getHours() + ":";
+  const mm = date.getMinutes() + ":";
+  const ss = date.getSeconds();
+  return Y + M + D + hh + mm + ss;
 }
 
